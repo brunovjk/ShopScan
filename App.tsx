099384d9c4ce679e850b3fccd86eb5407d48dd3b
brunovjk <brunovjk@gmail.com>
2024-06-04 12:29:17 -0300

    update MVP style

--- App.tsx
+++ App.tsx
@@ -1,72 +1,102 @@
 import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Provider as PaperProvider, Button, Text } from 'react-native-paper';
-// import Onyx, { useOnyx } from 'react-native-onyx';
-import { StatusBar } from 'expo-status-bar';
+import Onyx, { useOnyx } from 'react-native-onyx';
+import { MaterialIcons } from '@expo/vector-icons';
 
-// const ONYXKEYS = {
-//   COUNTER: 'counter',
-// };
+const ONYXKEYS = {
+  COUNTER: 'counter',
+};
 
-// Onyx.init({ keys: ONYXKEYS });
+Onyx.init({
+  keys: ONYXKEYS,
+});
 
 export default function App() {
-  // const [_value, _status] = useOnyx(ONYXKEYS.COUNTER);
+  const [_value, _status] = useOnyx(ONYXKEYS.COUNTER);
 
-  const [counter, setCounter] = React.useState<{
-    value: number;
-    status: 'loading' | 'loaded';
-  }>({
-    value: 0,
-    status: 'loaded'
-  });
-  // const value = (_value as { value: number })?.value;
-  // const status = (_status as { status: 'loading' | 'loaded' })?.status;
+  const value = (_value as { value: number })?.value;
+  const status = (_status as { status: 'loading' | 'loaded' })?.status;
 
   const incrementCount = () => {
-    if (counter.status !== 'loaded') { return; }
-    setCounter((prevState) => ({
-      ...prevState,
-      value: prevState.value + 1
-    }));
-    // Onyx.set(ONYXKEYS.COUNTER, { value: value + 1 });
+    if (status !== 'loaded') { return; }
+    Onyx.set(ONYXKEYS.COUNTER, { value: value + 1 });
   };
 
   const resetCount = () => {
-    if (counter.status !== 'loaded') { return; }
-    setCounter((prevState) => ({
-      ...prevState,
-      value: 0
-    }));
-    // Onyx.set(ONYXKEYS.COUNTER, { value: 0 });
+    if (status !== 'loaded') { return; }
+    Onyx.set(ONYXKEYS.COUNTER, { value: 0 });
   };
 
   return (
     <PaperProvider>
-      <View style={styles.container}>
-        {counter.status === 'loading' ? (
-          <Text>Loading counter...</Text>
-        ) : (
-          <>
-            <Text>Counter Value: {counter.value}</Text>
-            <Button mode="contained" onPress={incrementCount}>
-              Increment Counter
-            </Button>
-            <Button mode="contained" onPress={resetCount}>
-              Reset Counter
-            </Button>
-          </>
-        )}
+      <View style={styles.phoneContainer}>
+        <View style={styles.container}>
+          {status === 'loading' ? (<Text style={styles.loadingText}>Loading counter...</Text>) : (
+            <>
+              <View style={styles.counterContainer}>
+                <MaterialIcons name="timer" size={32} color="#FFA500" />
+                <Text style={styles.counterText}>Counter Value: {value}</Text>
+              </View>
+
+              <View style={styles.buttonContainer}>
+                <Button
+                  mode="contained"
+                  onPress={incrementCount}
+                  icon={() => <MaterialIcons name="add" size={24} color="white" />}
+                  style={[styles.button, { backgroundColor: '#1ca658' }]}
+                >
+                  Increment
+                </Button>
+                <Button
+                  mode="contained"
+                  onPress={resetCount}
+                  icon={() => <MaterialIcons name="refresh" size={24} color="white" />}
+                  style={[styles.button, { backgroundColor: '#3F8EFF' }]}
+                >
+                  Reset
+                </Button>
+              </View>
+            </>
+          )}
+        </View>
       </View>
-      <StatusBar style="auto" />
     </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  phoneContainer: {
+    padding: 48,
+    margin: 'auto',
+    borderWidth: 1,
+    borderRadius: 16,
+    borderColor: 'lightgray',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  loadingText: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  counterContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
+    marginBottom: 20,
+  },
+  counterText: {
+    fontSize: 24,
+    marginLeft: 10,
+  },
+  buttonContainer: {
+    gap: 16,
+    flexDirection: 'column',
+  },
+  button: {
+    borderRadius: 25,
+    paddingHorizontal: 20,
   },
 });
