54b9b534d0b72ce7f270fabc9f3025f0b915313e
Bruno Rocha <95647348+brunovjk@users.noreply.github.com>
2025-07-24 11:54:55 -0300

    Show empty list message

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,11 +4,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:shopscan/main.dart';
 
 void main() {
-  testWidgets('Lista de itens exibida', (WidgetTester tester) async {
+  testWidgets('Mensagem de lista vazia exibida', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
     await tester.pumpAndSettle();
 
-    expect(find.text('Produto 1'), findsOneWidget);
+    expect(find.text('Lista vazia, adicione ou escaneie um produto.'),
+        findsOneWidget);
     expect(find.text('Tela de Câmera (em breve)'), findsNothing);
 
     await tester.tap(find.byIcon(Icons.camera_alt));
