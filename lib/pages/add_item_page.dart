24305fb83cae6ca8930391f96a40ccc2042b9a51
Bruno Rocha <95647348+brunovjk@users.noreply.github.com>
2025-07-29 20:06:58 -0300

    Integrate camera and OCR scanning

--- lib/pages/add_item_page.dart
+++ lib/pages/add_item_page.dart
@@ -3,7 +3,10 @@ import '../data/item.dart';
 import '../data/items_database.dart';
 
 class AddItemPage extends StatefulWidget {
-  const AddItemPage({super.key});
+  const AddItemPage({super.key, this.initialName, this.initialPrice});
+
+  final String? initialName;
+  final double? initialPrice;
 
   @override
   State<AddItemPage> createState() => _AddItemPageState();
@@ -11,13 +14,26 @@ class AddItemPage extends StatefulWidget {
 
 class _AddItemPageState extends State<AddItemPage> {
   final _formKey = GlobalKey<FormState>();
-  final _nameController = TextEditingController();
-  final _quantityController = TextEditingController(text: '1');
+  late final TextEditingController _nameController;
+  late final TextEditingController _quantityController;
+  late final TextEditingController _priceController;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController(text: widget.initialName ?? '');
+    _quantityController = TextEditingController(text: '1');
+    _priceController = TextEditingController(
+        text: widget.initialPrice != null && widget.initialPrice! > 0
+            ? widget.initialPrice!.toString()
+            : '');
+  }
 
   @override
   void dispose() {
     _nameController.dispose();
     _quantityController.dispose();
+    _priceController.dispose();
     super.dispose();
   }
 
@@ -26,6 +42,7 @@ class _AddItemPageState extends State<AddItemPage> {
       final item = Item(
         name: _nameController.text,
         quantity: int.tryParse(_quantityController.text) ?? 1,
+        price: double.tryParse(_priceController.text) ?? 0,
       );
       await ItemsDatabase.instance.create(item);
       if (context.mounted) {
@@ -54,6 +71,11 @@ class _AddItemPageState extends State<AddItemPage> {
                 controller: _quantityController,
                 keyboardType: TextInputType.number,
                 decoration: const InputDecoration(labelText: 'Quantidade'),
+              ),
+              TextFormField(
+                controller: _priceController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(labelText: 'Preço'),
               ),
               const SizedBox(height: 20),
               ElevatedButton(
